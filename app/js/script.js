0dca822914cc248131e70b9b98480ed91c1692b5
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-08-09 22:42:02 +0100

    validation and bill splitting logics

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,107 @@
-console.log("Hello");
+const billWrapper = document.querySelector("#bill-wrapper");
+const bill = document.querySelector("#bill");
+
+const peopleWrapper = document.querySelector("#people-wrapper");
+const people = document.querySelector("#people");
+
+const percentWrapper = document.querySelector("#percent-wrapper");
+const radioButtons = document.getElementsByName("percentage");
+const customInput = document.querySelector("#customValue");
+
+// Get all input and add event listener
+const inputs = document.querySelectorAll("input");
+
+for (let index = 0; index < inputs.length; index++) {
+  inputs[index].onchange = calculateTip;
+}
+
+// Get bill value
+function getBillValue() {
+  return bill.value;
+}
+
+// Get percentage of tip
+function getTipPercentage() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      return radioButtons[i].value;
+    }
+  }
+  return null;
+}
+
+// Get number of people splliting the bill
+function getNumberOfPeople() {
+  return people.value;
+}
+
+// Show error message on an input-group
+function showError(element, errorMessage) {
+  element.classList.add("error");
+  element.querySelector(".label-wrap .error-message").textContent =
+    errorMessage;
+}
+
+// remove error message from an input group
+function removeError() {
+  for (let index = 0; index < arguments.length; index++) {
+    arguments[index].classList.remove("error");
+  }
+}
+
+// Check if value is valid then, convert to number
+function checkAndConvert(value, inputWrapper) {
+  if (value == "" || value == " ") {
+    showError(inputWrapper, "enter number");
+  } else if (isNaN(Number(value))) {
+    showError(inputWrapper, "enter valid number");
+  } else if (Number(value) == 0) {
+    showError(inputWrapper, "Can't be zero");
+  } else {
+    return Number(value);
+  }
+  return null;
+}
+
+// Calculate tip
+function calculateTip() {
+  console.log("calculate tip started");
+
+  // Get bill, percentage to tip and number of people splitting the bill
+  let billValue = getBillValue();
+  let tipPercentage = getTipPercentage();
+  let numOfPeople = getNumberOfPeople();
+
+  // Removing all previous error messages if all values are valid
+  removeError(billWrapper, percentWrapper, peopleWrapper);
+
+  // Conversion and Error check for bill value
+  billValue = checkAndConvert(billValue, billWrapper);
+  if (!billValue) return;
+
+  // Error check for tip percentage
+  if (tipPercentage == null) {
+    showError(percentWrapper, "pick or enter percentage");
+    return;
+  } else {
+    tipPercentage = Number(tipPercentage);
+  }
+
+  // Error check for number of people splitting bill
+  numOfPeople = checkAndConvert(numOfPeople, peopleWrapper);
+  if (!numOfPeople) return;
+
+  // Calculating tip
+  console.log(tipPercentage / 100.0);
+  let tip = billValue * (tipPercentage / 100.0);
+  console.log(`tip`, tip);
+  let totalPerPerson = (billValue + tip) / numOfPeople;
+  let tipPerPerson = tip / numOfPeople;
+
+  // Approximating results to 2d.p
+  totalPerPerson = Math.round((totalPerPerson + Number.EPSILON) * 100) / 100;
+  tipPerPerson = Math.round((tipPerPerson + Number.EPSILON) * 100) / 100;
+
+  // Displaying results
+  
+}
